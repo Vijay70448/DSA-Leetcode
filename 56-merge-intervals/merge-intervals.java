dad0ddb3816f79838c3e6414408de707db1c73a6
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2025-08-27 23:29:03 +0530

    Time: 9 ms (44.21%) | Memory: 46.8 MB (26.06%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -4,22 +4,31 @@ class Solution {
         int nums[][]=new int[arr.length][2];
         int k=0;
         for(int i=0;i<arr.length;i++){
-            int start=arr[i][0];
-            int end=arr[i][1];
-            if(k>0&& nums[k-1][1]>=end){
-                continue;
+            // int start=arr[i][0];
+            // int end=arr[i][1];
+            // if(k>0&& nums[k-1][1]>=end){
+            //     continue;
+            // }
+            // for(int j=i+1;j<arr.length;j++){
+            //     if(arr[j][0]<=end){
+            //         end=Math.max(end,arr[j][1]);
+            //     }
+            //     else{
+            //         break;
+            //     }
+            // }
+            // nums[k][0]=start;
+            // nums[k][1]=end;
+            // k++;
+
+            if(k==0 || nums[k-1][1]<arr[i][0]){
+                nums[k][0]=arr[i][0];
+                nums[k][1]=arr[i][1];
+                k++;
             }
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[j][0]<=end){
-                    end=Math.max(end,arr[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                nums[k-1][1]=Math.max(nums[k-1][1],arr[i][1]);
             }
-            nums[k][0]=start;
-            nums[k][1]=end;
-            k++;
         }
         return Arrays.copyOfRange(nums,0,k);
 
