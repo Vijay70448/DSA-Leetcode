3e89605390afd680d232104317281f27d539396f
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2025-08-28 11:26:57 +0530

    Time: 7 ms (98.65%) | Memory: 46.5 MB (73.55%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,37 +1,27 @@
 class Solution {
     public int[][] merge(int[][] arr) {
         Arrays.sort(arr,(a,b)->a[0]-b[0]);
-        int nums[][]=new int[arr.length][2];
+        int nums[][]= new int[arr.length][2];
         int k=0;
         for(int i=0;i<arr.length;i++){
-            // int start=arr[i][0];
-            // int end=arr[i][1];
-            // if(k>0&& nums[k-1][1]>=end){
-            //     continue;
-            // }
-            // for(int j=i+1;j<arr.length;j++){
-            //     if(arr[j][0]<=end){
-            //         end=Math.max(end,arr[j][1]);
-            //     }
-            //     else{
-            //         break;
-            //     }
-            // }
-            // nums[k][0]=start;
-            // nums[k][1]=end;
-            // k++;
-
-            if(k==0 || nums[k-1][1]<arr[i][0]){
-                nums[k][0]=arr[i][0];
-                nums[k][1]=arr[i][1];
-                k++;
+            int start=arr[i][0];
+            int end=arr[i][1];
+            if(k>0 && end<=nums[k-1][1]){
+                continue;
             }
-            else{
-                nums[k-1][1]=Math.max(nums[k-1][1],arr[i][1]);
+            for(int j=i+1;j<arr.length;j++){
+                if(end>=arr[j][0]){
+                    end=Math.max(end,arr[j][1]);
+                }
+                else{
+                    break;
+                }
             }
+            nums[k][0]=start;
+            nums[k][1]=end;
+            k++;
         }
         return Arrays.copyOfRange(nums,0,k);
-
         
     }
 }
