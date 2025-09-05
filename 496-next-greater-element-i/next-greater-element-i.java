08bdfe564cd4ec32d1dffdd84b78e30dffddd197
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2025-09-05 11:48:14 +0530

    Time: 3 ms (91.02%) | Memory: 44.5 MB (26.22%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,30 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums2.length;i++){
-            int count=0;
-            for(int j=i+1;j<nums2.length;j++){
-                if(nums2[i]<nums2[j]){
-                    count=nums2[j];
-                    break;
+        Stack<Integer> st = new Stack<>();
+        int arr[]=new int[nums2.length];
+        for(int i=nums2.length-1;i>=0;i--){
+                while(!st.isEmpty() && st.peek()<=nums2[i]){
+                    st.pop();
                 }
-            }
-            map.put(nums2[i],count);
+                if(st.isEmpty()){
+                    arr[i]=-1;
+                }
+                else{
+                    arr[i]=st.peek();
+                }
+                st.push(nums2[i]);
         }
-        ArrayList<Integer> list = new ArrayList<>();
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(nums2[i],arr[i]);
+        }
+        int nge[]=new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
-            if(map.get(nums1[i])==0){
-                list.add(-1);
-            }
-            else{
-                list.add(map.get(nums1[i]));
-            }
+            nge[i]=map.get(nums1[i]);
         }
-        int arr[]= new int[list.size()];
-        int k=0;
-        for(int key:list){
-            arr[k++]=key;
-        }
-        return arr;
+        return nge;
+        
     }
 }
