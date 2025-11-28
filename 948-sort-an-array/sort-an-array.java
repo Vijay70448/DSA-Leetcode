df39b1472706a6502fddfd251368f56f1b678b22
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2025-11-28 20:09:26 +0530

    Time: 21 ms (84.32%) | Memory: 83.3 MB (18.92%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Queue<Integer> pq= new PriorityQueue<>();
-        for(int i=0;i<nums.length;i++){
-            pq.add(nums[i]);
-        }
-        int j=0;
-        while(!pq.isEmpty()){
-            nums[j++]=pq.poll();
-        }
+        // Queue<Integer> pq= new PriorityQueue<>();
+        // for(int i=0;i<nums.length;i++){
+        //     pq.add(nums[i]);
+        // }
+        // int j=0;
+        // while(!pq.isEmpty()){
+        //     nums[j++]=pq.poll();
+        // }
+        // return nums;
+        Arrays.sort(nums);
         return nums;
     }
 }
