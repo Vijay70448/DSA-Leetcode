e4b5187ec70504cab7717e73ebd6140f63b73766
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2026-03-22 22:27:28 +0530

    Time: 1 ms (43.87%) | Memory: 45.5 MB (9.43%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,57 +1,88 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        // boolean f1=true;
-        // for(int key:nums1){
-        //     if(key%2!=0){
-        //         f1=false;
-        //         break;
-        //     }
-        // }
-        // if(f1){
-        //     return true;
-        // }
+        boolean f1=true;
+        for(int key:nums1){
+            if(key%2!=0){
+                f1=false;
+                break;
+            }
+        }
+        if(f1){
+            return true;
+        }
 
-        // boolean f2=true;
-        // for(int key:nums1){
-        //     if(key%2==0){
-        //         f2=false;
-        //         break;
-        //     }
-        // }
-        // if(f2){
-        //     return true;
-        // }
+        boolean f2=true;
+        for(int key:nums1){
+            if(key%2==0){
+                f2=false;
+                break;
+            }
+        }
+        if(f2){
+            return true;
+        }
 
-        // int arr[]= new int[nums1.length];
-        // for(int i=0;i<nums1.length;i++){
-        //     if(nums1[i]%2!=0){
-        //         int a=0;
-        //         for(int j=0;j<nums1.length;j++){
-        //             if(i==j){
-        //                 continue;
-        //             }
-        //             a=nums1[i]-nums1[j];
-        //             if(a%2==0){
-        //                 arr[i]=a;
-        //                 break;
-        //             }
-        //         }
-        //     }
-        //     else{
-        //         arr[i]=nums1[i];
-        //     }
-        // }
-        // boolean flag1=true;
-        // for(int key:nums1){
-        //     if(key%2!=0){
-        //         flag1=false;
-        //         break;
-        //     }
-        // }
-        // if(flag1){
-        //     return true;
-        // }
-        return true;
+        int arr[]= new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            if(nums1[i]%2!=0){
+                int a=0;
+                for(int j=0;j<nums1.length;j++){
+                    if(i==j){
+                        continue;
+                    }
+                    a=nums1[i]-nums1[j];
+                    if(a%2==0){
+                        arr[i]=a;
+                        break;
+                    }
+                }
+            }
+            else{
+                arr[i]=nums1[i];
+            }
+        }
+        boolean flag1=true;
+        for(int key:nums1){
+            if(key%2!=0){
+                flag1=false;
+                break;
+            }
+        }
+        if(flag1){
+            return true;
+        }
+        
+        for(int i=0;i<nums1.length;i++){
+            if(nums1[i]%2==0){
+                int a=0;
+                for(int j=0;j<nums1.length;j++){
+                    if(i==j){
+                        continue;
+                    }
+                    a=nums1[i]-nums1[j];
+                    if(a%2!=0){
+                        arr[i]=a;
+                        break;
+                    }
+                }
+            }
+            else{
+                arr[i]=nums1[i];
+            }
+        }
+
+        boolean flag2=true;
+        for(int key:nums1){
+            if(key%2==0){
+                f2=false;
+                break;
+            }
+        }
+        if(flag2){
+            return true;
+        }
+
+        return false;
 
     }
 }
