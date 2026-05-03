fff7845859bf6fb6bd800580e42df7b37e68d1d0
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2026-05-03 11:01:30 +0530

    Time: 4 ms (75.86%) | Memory: 42.7 MB (69.27%) - LeetSync

--- 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.java
+++ 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.java
@@ -29,7 +29,7 @@ class Solution {
         if(num==2){
             return true;
         }
-        for(int i=2;i<num;i++){
+        for(int i=2;i*i<=num;i++){
             if(num%i==0){
                 return false;
             }
