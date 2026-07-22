451f216e9d109b55ba5e89d07a9374bb562518f3
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2026-07-22 18:57:40 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (72.83%) - LeetSync

---
+++ 4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String rearrangeString(String s, char x, char y) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+            char ch= s.charAt(i);
+            if(ch==y){
+                sb.append(y);
+            }
+        }
+        
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch!=y){
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+
+    }
+}
