51fdce1cccd6d887a4d028c5b42a45abda8f1798
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2026-08-26 21:33:38 +0530

    Time: 418 ms (48.51%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select email from person group by email having count(*)>1;
+select email as Email from person where email IS NOT NULL 
+group by email having count(email)>1;
