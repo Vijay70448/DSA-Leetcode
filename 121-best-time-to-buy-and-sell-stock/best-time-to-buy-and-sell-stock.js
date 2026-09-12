4cb8a7dff1b706314fd9c007284b2cc5ae328a60
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2026-09-12 18:57:53 +0530

    Time: 3 ms (59.04%) | Memory: 64.5 MB (84.73%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+
+    let min=prices[0];
+    let profit=0;
+    for(let i=1;i<prices.length;i++){
+        if(prices[i]>min){
+            let a = prices[i]-min;
+            if(a>profit){
+                profit=a;
+            }
+        }
+        if(prices[i]<min){
+            min=prices[i];
+        }
+    }
+    return profit;
+
+    
+};
