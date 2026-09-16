c002d0874a3f8b810a06f3c361c75b1121728cbc
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2026-09-16 10:29:55 +0530

    Time: 12 ms (24.56%) | Memory: 60.8 MB (33.76%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement = function(nums) {
+
+    let map= new Map();
+    for(let i=0;i<nums.length;i++){
+        let a=nums[i];
+        if(map.has(a)){
+            map.set(a,map.get(a)+1);
+        }
+        else{
+            map.set(a,1);
+        }
+    }
+    let arr=new Array();
+    for(let key of map.keys()){
+        let a=map.get(key);
+        if(a>(nums.length/3)){
+            arr.push(key);
+        }
+    }
+    return arr;
+    
+};
