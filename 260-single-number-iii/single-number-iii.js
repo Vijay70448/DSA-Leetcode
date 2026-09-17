298b67f95fb7af10254fd6883c874353c2dbaad6
Vijay kumar <162294844+Vijay70448@users.noreply.github.com>
2026-09-17 10:51:52 +0530

    Time: 9 ms (23.53%) | Memory: 59 MB (14.97%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function(nums) {
+
+    let map = new Map();
+
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i])){
+            map.set(nums[i],map.get(nums[i])+1);
+        }
+        else{
+            map.set(nums[i],1);
+        }
+    }
+
+    let arr = new Array();
+    for(let key of map.keys()){
+        if(map.get(key)==1){
+            arr.push(key);
+        }
+    }
+
+    return arr;
+    
+};
